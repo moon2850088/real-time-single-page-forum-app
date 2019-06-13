3720ac2095e969fa67753393406bb3c6072ca4fe
moon2850088 <zhou33750@gmail.com>
2019-06-13 17:46:23 -0400

    Relationships

--- app/Model/Reply.php
+++ app/Model/Reply.php
@@ -3,8 +3,22 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Reply extends Model
 {
-    //
+    public function question(){
+
+        return $this->belongTo(Question::class);
+    }
+
+    public function user(){
+
+        return $this->belongTo(User::class);
+    }
+
+    public function like(){
+
+        return $this->hasMany(Like::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -28,6 +28,11 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    public function question(){
+
+        return $this->hasMany(Question::class);
+    }
+
     /**
      * The attributes that should be cast to native types.
      *
